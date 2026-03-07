56e56f720050d7fc9c659791076bcf1aa37a3b9a
fgarcia <gfabian757@gmail.com>
2026-03-07 13:46:28 -0600

    feat add struct definitions

--- programs/coffee-shop-solana/src/lib.rs
+++ programs/coffee-shop-solana/src/lib.rs
@@ -1,16 +1,51 @@
 use anchor_lang::prelude::*;
-
 declare_id!("8QNsjr6drj8ptAAmrZybaubn2U91xRY93taqobuU43G8");
 
-#[program]
-pub mod coffee_shop_solana {
+#[program] 
+pub mod coffe{
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_coffee_shop() -> Result<()> {
         Ok(())
     }
+
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+
+#[derive(InitSpace)] 
+pub struct CoffeeShop {
+    pub owner: Pubkey,
+     #[max_len(60)]
+    pub name: String,
+    pub total_orders: u64,
+}
+
+#[account]
+#[derive(InitSpace)] 
+pub struct MenuItem {
+    pub shop: Pubkey,
+    #[max_len(30)]
+    pub name: String,
+    pub price: u64,
+    pub available: bool,
+}
+
+#[account]
+#[derive(InitSpace)] 
+pub struct Order {
+    pub shop: Pubkey,
+    pub customer: Pubkey,
+    #[max_len(5)] // Assuming a maximum of 5 items per order
+    pub items: Vec<OrderItem>,
+    pub total_price: u64,
+    pub timestamp: i64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, InitSpace, PartialEq, Debug)]
+pub struct OrderItem {
+    pub menu_item: Pubkey,
+    pub price: u64,
+    #[max_len(3)] // Assuming a maximum quantity of 3 for any item
+    pub quantity: u8,
+}
